f051435c089c4788f7eee912143a814b064c92e2
Michael Watts <wattsjmichael@gmail.com>
2020-10-30 08:29:50 -0700

    Creates Framework with proper naming conventions

--- ParkWatchAPI/Program.cs
+++ ParkWatchAPI/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Parkwatchapi
+namespace ParkWatchApi
 {
     public class Program
     {
