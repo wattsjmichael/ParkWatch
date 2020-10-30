3a63f916832f9f0ab5c0f68806852faed79754b9
Michael Watts <wattsjmichael@gmail.com>
2020-10-30 10:05:30 -0700

    Adds Get and Post to NatlParksController.cs

---
+++ ParkWatchAPI/Controllers/NatlParksController.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ParkWatchApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace ParkWatchApi.Controllers
+{
+[Route("api/[controller]")]
+  [ApiController]
+  public class NatlParksController : ControllerBase
+  {
+    private ParkWatchApiContext _db;
+
+    public NatlParksController(ParkWatchApiContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/NatlParks
+    [HttpGet]
+    public ActionResult<IEnumerable<NatlPark>> Get()
+    {
+      return _db.NatlParks.ToList();
+    }
+
+    // POST api/natlparks
+    [HttpPost]
+    public void Post([FromBody] NatlPark natlPark)
+    {
+      _db.NatlParks.Add(natlPark);
+      _db.SaveChanges();
+    }
+
+    //Get Api/natlparks/2
+    [HttpGet("{id}")]
+    public ActionResult<NatlPark> Get(int id)
+    {
+      return _db.NatlParks.FirstOrDefault(entry=>entry.NatlParkId == id);
+    }
+  }
+}
