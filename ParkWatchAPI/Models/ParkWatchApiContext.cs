c79e7a023b5dd90cf33dd0b5c595ca74f564fe47
Michael Watts <wattsjmichael@gmail.com>
2020-10-30 11:11:02 -0700

    adds Seeddata for NatlPark and StateParks

--- ParkWatchAPI/Models/ParkWatchApiContext.cs
+++ ParkWatchAPI/Models/ParkWatchApiContext.cs
@@ -2,14 +2,34 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ParkWatchApi.Models
 {
-    public class ParkWatchApiContext : DbContext
+  public class ParkWatchApiContext : DbContext
+  {
+    public ParkWatchApiContext(DbContextOptions<ParkWatchApiContext> options)
+        : base(options)
     {
-        public ParkWatchApiContext(DbContextOptions<ParkWatchApiContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<StatePark> StateParks { get; set; }
-        public DbSet<NatlPark> NatlParks { get; set ;}
     }
+
+    public DbSet<StatePark> StateParks { get; set; }
+    public DbSet<NatlPark> NatlParks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<StatePark>()
+      .HasData(
+       new StatePark { StateParkId = 1, StateParkName = "Bellingham State Park", StateParkCity = "Bellingham", StateParkState = "Washington", StateParkCampingSpots = 15, IsOpenState = true },
+       new StatePark { StateParkId = 2, StateParkName = "Cascadia Park", StateParkCity = "Vancouver", StateParkState = "Washington", StateParkCampingSpots = 4, IsOpenState = true },
+       new StatePark { StateParkId = 3, StateParkName = "Seaside State Park", StateParkCity = "Seaside", StateParkState = "Oregon", StateParkCampingSpots = 0, IsOpenState = false },
+       new StatePark { StateParkId = 4, StateParkName = "Mt Hood State Park", StateParkCity = "Governement Camp", StateParkState = "Oregon", StateParkCampingSpots = 6, IsOpenState = true },
+       new StatePark { StateParkId = 1, StateParkName = "Bellingham State Park", StateParkCity = "Bellingham", StateParkState = "Washington", StateParkCampingSpots = 15, IsOpenState = true }
+      );
+      builder.Entity<NatlPark>()
+      .HasData(
+        new NatlPark { NatlParkId = 1, NatlParkName = "Yellowstone", NatlParkCity = "Yellowstone City", NatlParkState = "Wyoming", NatlParkCampingSpots = 200, IsOpenNatl = true },
+        new NatlPark { NatlParkId = 2, NatlParkName = "Glacier", NatlParkCity = "Glacier City", NatlParkState = "Montana", NatlParkCampingSpots = 1200, IsOpenNatl = true },
+        new NatlPark { NatlParkId = 3, NatlParkName = "Appalachia National Park", NatlParkCity = "Montiplier", NatlParkState = "Vermont", NatlParkCampingSpots = 25, IsOpenNatl = false },
+        new NatlPark { NatlParkId = 4, NatlParkName = "Mt. Ranier National Park", NatlParkCity = "Sequim", NatlParkState = "Washington", NatlParkCampingSpots = 300, IsOpenNatl = true },
+        new NatlPark { NatlParkId = 5, NatlParkName = "Cascade Rainforest", NatlParkCity = "Forks", NatlParkState = "Washington", NatlParkCampingSpots = 21, IsOpenNatl = false }
+      );
+    }
+  }
 }
