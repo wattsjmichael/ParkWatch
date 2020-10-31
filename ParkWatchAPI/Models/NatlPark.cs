e0742b25a4e7bfb37ad8dafebee7eaeb5375a8f0
Michael Watts <wattsjmichael@gmail.com>
2020-10-31 00:13:30 -0700

    Add required attribute to Models

--- ParkWatchAPI/Models/NatlPark.cs
+++ ParkWatchAPI/Models/NatlPark.cs
@@ -1,14 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 namespace ParkWatchApi.Models
 {
     public class NatlPark
     {
         public int NatlParkId { get; set; }
+        [Required]
         public string NatlParkName { get; set; }
+        [Required]
         public string NatlParkCity { get; set; }
+        [Required]
         public string NatlParkState { get; set; }
-
+        [Required]
         public int NatlParkCampingSpots { get; set; }
-
+        [Required]
         public bool? IsOpenNatl {get; set; }
         
         
--- ParkWatchAPI/Models/StatePark.cs
+++ ParkWatchAPI/Models/StatePark.cs
@@ -1,14 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 namespace ParkWatchApi.Models
 {
     public class StatePark
     {
         public int StateParkId { get; set; }
+        [Required]
         public string StateParkName { get; set; }
+        [Required]
         public string StateParkCity { get; set; }
+        [Required]
         public string StateParkState { get; set; }
-
+        [Required]
         public int StateParkCampingSpots { get; set; }
-
+        [Required]
         public bool? IsOpenState {get; set;}
         
 
